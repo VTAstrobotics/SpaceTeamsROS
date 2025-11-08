ebe448702e4bb5adfdfbb9078ab939a7b2a68c0d
Samuel Sleshi <samuelsleshi2@gmail.com>
2025-11-08 18:01:46 -0500

    Fixed

--- space_teams_python/space_teams_python/example_client.py
+++ space_teams_python/space_teams_python/example_client.py
@@ -70,7 +70,7 @@ class RoverController(Node):
 
         self.goal_cmd_rotation = msg.angular.z
         self.pid_rot.setpoint = self.goal_cmd_vel
-        setpoint = self.pid_rot(self.current_rotation_localFrame)
+        setpoint = self.pid_rot(float(self.current_velocity_localFrame.y))
         self.send_steer_command(setpoint)
     
     def location_marsFrame_callback(self, msg):
